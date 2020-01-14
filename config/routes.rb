fbd4dd9ba18e59139c948f2ac960cf573c7a2e7c
Eduardo Ramos <eduardoramos@Eduardos-MacBook-Pro.local>
2020-01-14 10:13:29 -0600

    1-14-20

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 Rails.application.routes.draw do
+  post 'add_to_cart' => 'cart#add_to_cart'
+
+  get 'view_order' => 'cart#view_order'
+
+  get 'checkout' => 'cart#checkout'
+  
+  devise_for :users
+ # get 'storefront/all_items'
+  #get 'storefront/items_by_category'
+  #get 'storefront/items_by_brand'
+    root 'storefront#all_items'
+
+  get 'categorical' => 'storefront#items_by_category' 
+
+  get 'branding' => 'storefront#items_by_brand'
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
